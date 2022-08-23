e64716b78a8122cb7875879de4c9a8ffae3b6e92
nstppbl <1nstppbl@gmail.com>
2022-08-23 18:50:10 +0500

    initialize float variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        float a, b;
-        (9 * ( a * a) - 5 * b + 2 + a - 7) * ((a + b- 4 * a * b) / 2);
+        float a = 0, b = 0;
+        float v = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
